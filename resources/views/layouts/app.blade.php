dae75e5a47cf1b977a39ce4713ea07eec40cbd7e
hammad7491 <madi.rock7491@gmail.com>
2025-07-19 10:40:21 -0700

    New Changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,8 @@
 <!DOCTYPE html>
 <html lang="en" data-theme="light">
 
+{{-- this is head --}}
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
